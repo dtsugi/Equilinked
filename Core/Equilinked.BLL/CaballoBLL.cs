08943f370b6cd8dd0816a327979c281f2b8b60ac
Jesus alberto <jast1000@gmail.com>
2017-05-28 20:25:10 -0500

    Ajuste de importacion

--- Core/Equilinked.BLL/CaballoBLL.cs
+++ Core/Equilinked.BLL/CaballoBLL.cs
@@ -3,6 +3,7 @@ using Equilinked.DAL.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
